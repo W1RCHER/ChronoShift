73a137f05cdbd55ff6f87d48a607d28d09ed5ba1
W1RCHER <svkomarov2009@gmail.com>
2026-03-09 20:09:30 +0700

    Pause debug

--- game/views/pause_view.py
+++ game/views/pause_view.py
@@ -10,15 +10,26 @@ from game.config.settings import (
 
 
 class PauseView(arcade.View):
+    """Экран паузы."""
+
     def __init__(self, game_view: arcade.View) -> None:
         super().__init__()
         self.game_view = game_view
+        self.ui_camera: arcade.Camera2D | None = None
 
     def on_show_view(self) -> None:
         arcade.set_background_color(COLOR_BACKGROUND)
 
+        if self.ui_camera is None:
+            self.ui_camera = arcade.Camera2D()
+
+        self.ui_camera.position = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     def on_draw(self) -> None:
         self.clear()
+
+        if self.ui_camera is not None:
+            self.ui_camera.use()
 
         arcade.draw_text(
             "ПАУЗА",
