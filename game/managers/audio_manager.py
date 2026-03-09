588fc918cafc7fbdc4f4b30e28554f1fd639eed9
W1RCHER <svkomarov2009@gmail.com>
2026-03-09 17:40:35 +0700

    Added a code skeleton for using audio

---
+++ game/managers/audio_manager.py
@@ -0,0 +1,20 @@
+class AudioManager:
+
+    def __init__(self) -> None:
+        self.sound_enabled = True
+        self.music_enabled = True
+
+    def play_click(self) -> None:
+        pass
+
+    def play_pickup(self) -> None:
+        pass
+
+    def play_damage(self) -> None:
+        pass
+
+    def play_win(self) -> None:
+        pass
+
+    def play_lose(self) -> None:
+        pass
